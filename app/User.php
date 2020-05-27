ae2cba94c38f9efa80c17aee5f9034bbb5c7fd2f
tonbi21 <tonbi0521@gmail.com>
2020-05-27 10:36:52 +0000

    follow, unfollo

--- app/User.php
+++ app/User.php
@@ -39,10 +39,58 @@ class User extends Authenticatable
     
     //このユーザが所有する投稿。（ Micropostモデルとの関係を定義）
     public function microposts(){
-        return $this->hasmany(Micropost::class);
+        return $this->hasMany(Micropost::class);
+    }
+    
+    //このユーザがフォロー中のユーザ。（ Userモデルとの関係を定義）
+    public function followings(){
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    
+    //このユーザをフォロー中のユーザ。（ Userモデルとの関係を定義）
+    public function followers(){
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
     }
     
     public function loadRelationshipCounts(){
-        $this->loadCount('microposts');
+        $this->loadCount(['microposts', 'followings', 'followers']);
+    }
+    
+    //指定された $userIdのユーザをこのユーザがフォロー中であるか調べる。フォロー中ならtrueを返す。
+    public function is_following($userId){
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+    
+    public function follow($userId){
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 相手が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist || $its_me){
+            // すでにフォローしていれば何もしない
+            return false;
+        }else{
+           // 未フォローであればフォローする
+           $this->followings()->attach($userId);
+           return true;
+           
+        }
+    }
+    
+    public function unfollow($userId){
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 相手が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist && !$its_me){
+            // すでにフォローしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        }else{
+            // 未フォローであれば何もしない
+            return false;
+        }
     }
 }
--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -6,17 +6,13 @@
             <!--ユーザー情報-->
             @include('users.card')
         </aside>
+        
         <div class="col-sm-8">
             <!--タブ-->
             @include('users.navtabs')
-            
-            @if (Auth::id() == $user->id)
-                <!--投稿フォーム-->
-                @include('microposts.form')
-            @endif
-            
-            <!--投稿一覧-->
-            @include('microposts.microposts')
+            <!--ユーザー一覧-->
+            @include('users.users')
         </div>
     </div>
+
 @endsection
